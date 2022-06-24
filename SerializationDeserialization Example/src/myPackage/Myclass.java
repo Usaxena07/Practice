2a83c285fc0807d96366c0edb757bec8a3c454ce
Uday Saxena <102949443+Usaxena07@users.noreply.github.com>
2022-06-24 15:49:43 +0530

    Ex

---
+++ SerializationDeserialization Example/src/myPackage/Myclass.java
@@ -0,0 +1,60 @@
+package myPackage;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+import java.util.ArrayList;
+
+class Employee implements Serializable
+{
+	int id;
+	String name;
+	int salary;
+	
+	public Employee(int id, String name, int salary)
+	{
+		this.id=id;
+		this.name=name;
+		this.salary=salary;
+	}	
+}
+
+public class Myclass 
+{
+	public static void main(String[] args) throws IOException, ClassNotFoundException 
+	{
+		/* PART-1 : Serialization code */
+		/*
+		Employee emp = new Employee(101,"Meg",2000);
+		
+		//Object => FileOutputStream (Serialization)
+		FileOutputStream fout = new FileOutputStream("E:\\JAVA Full Stack\\My Files\\Employees.txt");
+		ObjectOutputStream out = new ObjectOutputStream(fout);
+		out.writeObject(emp);
+		out.close();
+		fout.close();
+		
+		System.out.println("Serialization is done");
+		
+		*/
+		/* Part-2 : Deserialization code */
+		
+		FileInputStream fis = new FileInputStream("E:\\JAVA Full Stack\\My Files\\Employees.txt");
+		ObjectInputStream ois = new ObjectInputStream(fis);
+		
+		Employee emp = (Employee)ois.readObject();
+		System.out.println("Id = "+emp.id);
+		System.out.println("Name = "+emp.name);
+		System.out.println("Salary = "+emp.salary);
+		
+		ois.close();
+		fis.close();
+		
+		System.out.println("Desirialization is done");
+		
+	}
+}
